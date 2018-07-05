f7f6661780ba186a34b7b3e8a6fec9514c65eb9f
AJ Ross <aj@lasersox.net>
2018-07-05 10:01:39 -0800

    added support for es6 and auto-restart.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-var http = require('http');
+import http from 'http'
 
 //create a server object:
 http.createServer(function (req, res) {
   if (req.method == 'POST') {
-      var body = '';
+      var body = ''
       req.on('data', function (data) {
-          body += data;
+          body += data
           // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
           if (body.length > 1e6) { 
               // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-              req.connection.destroy();
+              req.connection.destroy()
           }
-      });
+      })
       req.on('end', function () {
 
-          var POST = JSON.parse(body);
+          var POST = JSON.parse(body)
           // use POST
           console.log(POST)
-          res.write(JSON.stringify(POST)); //write a response to the client
-          res.end(); //end the response
-      });
+          res.write(JSON.stringify(POST)) //write a response to the client
+          res.end() //end the response
+      })
   }
-}).listen(8080); //the server object listens on port 8080
+}).listen(8080) //the server object listens on port 8080
